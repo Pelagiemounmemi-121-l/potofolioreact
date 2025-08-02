1281477d82c1f04d267220a3e378998b9352cdbc
DAME MOUNMEMI <pelagimounmemi@gmail.com>
2025-08-02 10:53:02 +0200

    version ameliorer de mon potofolio

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'aos/dist/aos.css';
+import { HashRouter } from "react-router-dom";
 
 import 'aos/dist/aos.css';
 import './index.css';
@@ -12,8 +13,8 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+     </HashRouter>
   </React.StrictMode>
 );
